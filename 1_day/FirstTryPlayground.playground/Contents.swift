db1954e01f2584efbc3f448c6ec07e4148a3212f
Coding24h <zhangzhengyu@coding24h.com>
2017-05-16 13:43:03 +0800

    playground初体验

--- 1_day/FirstTryPlayground.playground/Contents.swift
+++ 1_day/FirstTryPlayground.playground/Contents.swift
@@ -3,3 +3,13 @@
 import UIKit
 
 var str = "Hello, playground"
+let view = UIView(frame: CGRect(x: 0, y: 0, width:100 ,height:100))
+view.backgroundColor = UIColor.red;
+let button = UIButton(type:.contactAdd)
+view.addSubview(button)
+button.center = view.center;
+var sum = 0
+for i in 0..<9 {
+    sum += i
+}
+		
